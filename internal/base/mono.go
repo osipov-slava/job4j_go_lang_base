35d99623709272b4a0ad46c3f73354ae06bf9c86
Slava Osipov <skybright@mail.ru>
2026-05-26 21:07:58 +0300

    505560

---
+++ internal/base/mono.go
@@ -0,0 +1,28 @@
+package base
+
+func Mono(nums []int) bool {
+	size := len(nums)
+	if size == 0 || size == 1 {
+		return true
+	}
+	direction := 0 // 0 - equals, 1 - increase , -1 = decrease
+
+	if nums[0] < nums[size-1] {
+		direction = 1
+	} else if nums[0] > nums[size-1] {
+		direction = -1
+	}
+
+	for i := 1; i < size-1; i++ {
+		if direction == 0 && (nums[i-1] != nums[i] || nums[i] != nums[size-1]) {
+			return false
+		}
+		if direction == 1 && (nums[i-1] > nums[i] || nums[i] > nums[size-1]) {
+			return false
+		}
+		if direction == -1 && (nums[i-1] < nums[i] || nums[i] < nums[size-1]) {
+			return false
+		}
+	}
+	return true
+}
